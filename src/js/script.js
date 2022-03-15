163377bb11d89bf7d618797682567dd0e6a07222
Craig Gieselman <cgieselman1@mac.com>
2022-03-14 20:08:59 -0500

    Desktop Navigation works!

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,40 @@
-const el = document.querySelector("#masthead");
+/*
+  Listen for CSS position:sticky to happen and add a class to the sticky element
+  https://davidwalsh.name/detect-sticky
+*/
+const mh = document.querySelector("#masthead");
 const observer = new IntersectionObserver(
-  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
+  ([e]) => {
+    e.target.classList.toggle("is-sticky", e.intersectionRatio < 1);
+  },
   { threshold: [1] }
 );
 
-observer.observe(el);
+observer.observe(mh);
 
+/*
+  Add a class "shoved status" class to the header.masthead for Transition Sequencing
+  Because I am tinkering with the Off-Canvas Library display and animation
+*/
+
+const np = document.querySelector(".nameplate");
+
+document
+  .querySelector("#drawer-trigger")
+  .addEventListener("click", function () {
+    if (np.classList.contains("is-shoved")) {
+      np.classList.remove("is-shoved");
+      np.classList.add("is-not-shoved");
+    } else {
+      np.classList.add("is-shoved");
+      np.classList.remove("is-not-shoved");
+    }
+  });
+
+/*
+  Dark mode Toggling that came for free with the 11ty Template
+  https://github.com/NikitaZanella/11ty-SASS
+*/
 nightModeButton.addEventListener("click", darkModeHandler);
 nightModeDiv.addEventListener("load", darkModeState());
 
@@ -13,7 +42,6 @@ const nightModeButton = document.getElementById("nightModeButton");
 const nightModeDiv = document.getElementById("nightModeDiv");
 
 // Dark Mode Handler => switch theme on button click.
-
 const darkModeHandler = () => {
   if (localStorage.getItem("darkMode") == "1") {
     nightModeDiv.classList.remove("dark-theme");
@@ -26,7 +54,6 @@ const darkModeHandler = () => {
 
 // Dark Mode State Handler => check if a 'darkMode' local store exist.
 // If true, activate darkMode, else create a localStorage called 'darkMode'
-
 const darkModeState = () => {
   if (localStorage.getItem("darkMode") == "1") {
     nightModeDiv.classList.add("dark-theme");
